c2de0c59a80185baa74ebea6e52b07b7ebc1c4e0
bloonys <yanniening123@gmail.com>
2025-12-09 08:08:52 -0700

    create ionic project

---
+++ Cross Platform Development/firstIonic/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'firstIonic',
+  webDir: 'www'
+};
+
+export default config;
