8c5e51f59c005f8eecc4616730cdaa728312364f
bloonys <yanniening123@gmail.com>
2025-12-09 14:01:19 -0700

    create ionic project

---
+++ Cross Platform Development/travelApp/src/app/budget-friendly-tips/budget-friendly-tips.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-budget-friendly-tips',
+  templateUrl: './budget-friendly-tips.page.html',
+  styleUrls: ['./budget-friendly-tips.page.scss'],
+  standalone: false,
+})
+export class BudgetFriendlyTipsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ Cross Platform Development/travelApp/src/app/luxurious/luxurious.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-luxurious',
+  templateUrl: './luxurious.page.html',
+  styleUrls: ['./luxurious.page.scss'],
+  standalone: false,
+})
+export class LuxuriousPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
